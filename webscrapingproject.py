457990018e7d0a1a3d7dbdae1a2609b56b4dd1a8
Adam Craven <adamcraven8@gmail.com>
2023-03-07 18:49:14 +0000

    Created csv to push scraped data into

--- webscrapingproject.py
+++ webscrapingproject.py
@@ -2,9 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import numpy as np
+import csv
+import datetime
+import pandas as pd
 
 # ctrl + (] or [) to indent
 # ctrl + / to comment 
+
+# def check_price():
 
 html_text = requests.get('https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=Nintendo+Switch+Oled&_sacat=0&LH_TitleDesc=0&LH_Auction=1&LH_Sold=1&LH_Complete=1&_sop=13').text
 soup = BeautifulSoup(html_text, 'lxml')
@@ -12,6 +17,7 @@ soup = BeautifulSoup(html_text, 'lxml')
 products = soup.find_all('div', class_ = 's-item__info clearfix')[1:]
 # RegEx for retrieving floating numbers from a string
 pattern = r"\d*\.\d+|\d+\.\d*"
+today = datetime.date.today()
 '''
 print('Filter listings by condition')
 # Allow user to filter listings by chosen condition 
@@ -38,18 +44,42 @@ for product in products:
     # If not, default to 0.00
     else:
         postage_cost = 0.00
+
+    total_cost = price + postage_cost
     # If  chosen_condition exists in the condition variable, only show these listings 
     #if chosen_codition in condition:
 
+    # Create column headers for csv
+    header = ['Title', 'Condition', 'Price', 'Sale_Date', 'Postage', 'Total_Cost', 'Import_Date', 'URL']
+
+    # Specify data to import into columns
+    data = [title, condition, price, sale_date, postage_cost, total_cost, today, url]
+
+    # Append data to the csv
+    with open('eBayNintendoSwitchOledDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        # Write the header variable to the csv
+        # writer.writerow(header)
+        writer.writerow(data)
+
 
     # print all values
-    print(f'''
-    Title: {title}
-    Condition: {condition}
-    Price: {price}
-    Sale_Date: {sale_date}
-    Postage: {postage_cost}
-    URL: {url}
-    '''
-    )
+    # print(f'''
+    # Title: {title}
+    # Condition: {condition}
+    # Price: {price}
+    # Sale_Date: {sale_date}
+    # Postage: {postage_cost}
+    # URL: {url}
+    # '''
+    # )
 
+    
+#     while(True):
+#         check_price()
+#         time.sleep(5)
+
+# Read the csv 
+df = pd.read_csv('C:\\Users\\adamc\\Documents\\Python Project\\Ebay Webscraper\\eBayNintendoSwitchOledDataset.csv')
+
+print(df)
